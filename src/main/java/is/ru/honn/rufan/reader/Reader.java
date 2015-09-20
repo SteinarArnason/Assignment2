d12df32f6294bec3e69a1e18fb7ddfbb60c14369
Steinar Árnason <steinara13@ru.is>
2015-09-20 21:57:55 +0000

    Updating

--- src/main/java/is/ru/honn/rufan/reader/Reader.java
+++ src/main/java/is/ru/honn/rufan/reader/Reader.java
@@ -2,8 +2,8 @@ package is.ru.honn.rufan.reader;
 
 public interface Reader
 {
-    public Object read();
-    public Object parse(String content);
-    public void setURI(String URI);
-    public void setReadHandler(ReadHandler readHandler);
+    Object read();
+    Object parse(String content);
+    void setURI(String URI);
+    void setReadHandler(ReadHandler readHandler);
 }
