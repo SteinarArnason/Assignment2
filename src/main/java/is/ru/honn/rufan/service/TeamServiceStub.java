34a57a9c1e3e028e5c5d3e32dddde7db908ba023
kristinn93 <kristinnj13@ru.is>
2015-09-20 19:09:34 +0000

    implementing temservicestub

--- src/main/java/is/ru/honn/rufan/service/TeamServiceStub.java
+++ src/main/java/is/ru/honn/rufan/service/TeamServiceStub.java
@@ -3,20 +3,28 @@ package is.ru.honn.rufan.service;
 import is.ru.honn.rufan.domain.Team;
 import is.ru.honn.rufan.service.exception.ServiceException;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TeamServiceStub implements TeamService {
-
+    private Map<Integer, List<Team>> myMap;
     public TeamServiceStub() {
-
+        myMap = new HashMap<Integer, List<Team>>();
     }
 
     public int addTeam(int leagueId, Team team) throws ServiceException {
-        return 0;
+        if(!myMap.containsKey(leagueId)) {
+            myMap.put(leagueId, new ArrayList<Team>());
+        }
+        if(myMap.get(leagueId).contains(team)) {
+            throw new ServiceException();
+        }
+        else {
+            myMap.get(leagueId).add(team);
+        }
+        return team.getTeamId();
     }
 
     public List<Team> getTeams(int leagueId) {
-        return null;
+        return myMap.get(leagueId);
     }
 }
