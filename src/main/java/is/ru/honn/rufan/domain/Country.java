b84cb0c668ae65bd2c45b644b4193f8c5d347278
kristinn93 <kristinnj13@ru.is>
2015-09-20 16:46:58 +0000

    added country and player

---
+++ src/main/java/is/ru/honn/rufan/domain/Country.java
@@ -0,0 +1,37 @@
+package is.ru.honn.rufan.domain;
+
+/**
+ * Created by kristinn on 20/09/15.
+ */
+public class Country {
+    protected int countryId;
+    protected String name;
+    protected String abbreviation;
+
+    public Country() {
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+}
